ed461ca6cde55cc7ec58b9b41a808437fc052597
yupf1989 <929953483@qq.com>
2018-06-08 11:41:40 +0800

    编写个人主页布局

---
+++ app/src/main/java/com/rain/mymvpdemo/ui/activity/ProfileHomePageActivity.java
@@ -0,0 +1,28 @@
+package com.rain.mymvpdemo.ui.activity;
+
+import android.os.Bundle;
+
+import com.rain.mymvpdemo.R;
+import com.rain.mymvpdemo.base.BaseActivity;
+
+/**
+ * Author:rain
+ * Date:2018/6/8 9:38
+ * Description:
+ */
+public class ProfileHomePageActivity extends BaseActivity {
+    @Override
+    protected void loadData() {
+
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_profile_homepage;
+    }
+
+    @Override
+    public void initViews(Bundle savedInstanceState) {
+
+    }
+}
--- app/src/main/java/com/rain/mymvpdemo/ui/fragment/MineTabView.java
+++ app/src/main/java/com/rain/mymvpdemo/ui/fragment/MineTabView.java
@@ -1,13 +1,22 @@
 package com.rain.mymvpdemo.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.rain.mymvpdemo.R;
 import com.rain.mymvpdemo.base.BaseFragment;
-import com.rain.mymvpdemo.base.BaseListFragment;
+import com.rain.mymvpdemo.ui.activity.ProfileHomePageActivity;
 
-import java.util.List;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Author:rain
@@ -16,6 +25,29 @@ import java.util.List;
  */
 public class MineTabView extends BaseFragment {
 
+    @BindView(R.id.iv_about)
+    ImageView ivAbout;
+    @BindView(R.id.iv_avatar)
+    CircleImageView ivAvatar;
+    @BindView(R.id.tv_nickname)
+    TextView tvNickname;
+    @BindView(R.id.tv_view_homepage)
+    TextView tvViewHomepage;
+    @BindView(R.id.tv_collection)
+    TextView tvCollection;
+    @BindView(R.id.tv_comment)
+    TextView tvComment;
+    @BindView(R.id.tv_mine_message)
+    TextView tvMineMessage;
+    @BindView(R.id.tv_mine_attention)
+    TextView tvMineAttention;
+    @BindView(R.id.tv_mine_cache)
+    TextView tvMineCache;
+    @BindView(R.id.tv_watch_history)
+    TextView tvWatchHistory;
+    @BindView(R.id.tv_feedback)
+    TextView tvFeedback;
+    Unbinder unbinder;
 
     @Override
     protected int getLayoutId() {
@@ -29,5 +61,41 @@ public class MineTabView extends BaseFragment {
 
     public static MineTabView newInstance() {
         return new MineTabView();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick({R.id.iv_about, R.id.iv_avatar, R.id.tv_nickname, R.id.tv_view_homepage, R.id.tv_mine_message, R.id.tv_mine_attention, R.id.tv_mine_cache, R.id.tv_watch_history, R.id.tv_feedback})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.iv_about:
+                break;
+            case R.id.iv_avatar:
+            case R.id.tv_nickname:
+            case R.id.tv_view_homepage:
+                startActivity(new Intent(getActivity(), ProfileHomePageActivity.class));
+                break;
+            case R.id.tv_mine_message:
+                break;
+            case R.id.tv_mine_attention:
+                break;
+            case R.id.tv_mine_cache:
+                break;
+            case R.id.tv_watch_history:
+                break;
+            case R.id.tv_feedback:
+                break;
+        }
     }
 }
